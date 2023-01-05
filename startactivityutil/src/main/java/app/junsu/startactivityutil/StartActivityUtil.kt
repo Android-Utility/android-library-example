e7fc53170708a03b0b4b23561da35c74a74aacb2
junsuPark <krpjs0508@gmail.com>
2023-01-05 20:19:14 +0900

    test commit

---
+++ startactivityutil/src/main/java/app/junsu/startactivityutil/StartActivityUtil.kt
@@ -0,0 +1,66 @@
+package app.junsu.startactivityutil
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
+object StartActivityUtil {
+
+    private fun <T : Activity> Activity.startActivity(
+        context: Context, to: Class<out T>,
+        flag: List<Int> = listOf(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+    ) {
+        startActivity(
+            Intent(
+                /* packageContext = */
+                context,
+                /* cls = */
+                to,
+            ).apply {
+                flag.forEach {
+                    this.addFlags(
+                        /* flags = */
+                        it,
+                    )
+                }
+            })
+    }
+
+    /**
+     * @author junsuPark
+     * default public startActivity function, which lets context activity maintain single activity.
+     */
+    fun <T : Activity> Activity.startActivity(
+        context: Context,
+        to: Class<out T>,
+    ) {
+        startActivity(
+            context = context,
+            to = to,
+            flag = listOf(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+        )
+    }
+
+    fun <T : Activity> Activity.startActivityFinishingCurrentActivity(
+        context: Context, to: Class<out T>,
+        flag: List<Int> = listOf(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+    ) {
+        startActivity(
+            context = context,
+            to = to,
+            flag = flag,
+        )
+        finish()
+    }
+
+    fun <T : Activity> Activity.startActivityRemovingBackStack(
+        context: Context,
+        to: Class<out T>,
+    ) {
+        startActivityFinishingCurrentActivity(
+            context = context,
+            to = to,
+            flag = listOf(Intent.FLAG_ACTIVITY_CLEAR_TASK, Intent.FLAG_ACTIVITY_NEW_TASK),
+        )
+    }
+}
